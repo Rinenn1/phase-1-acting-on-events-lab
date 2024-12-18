182475fcf1e75994f9cf5fe27a9ebf13541fe713
Isaac <bennisaac24@gmail.com>
2024-12-18 19:09:56 +0300

    code challenge complete

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+
+const container = document.getElementById("game");
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left + 40 < 400) {
+      dodger.style.left = `${left + 1}px`;
+    }
+}
+  
+document.addEventListener("keydown", (event) => {
+    if (event.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (event.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+});
